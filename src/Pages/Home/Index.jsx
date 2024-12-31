beb1205e8b26e716f3dd1a86587851c257d42448
Pedro Vieira <ppgvsilva@gmail.com>
2024-12-31 15:38:22 -0300

    Section Card Responsivo

--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -1,12 +1,14 @@
 import Footer from "../../components/Footer";
 import Main from "../../components/Main/Index";
 import Navbar from "../../components/Navbar";
+import SectionCards from "../../components/SectionCards/Index";
 
 const Home = () => {
   return (
     <>
     <Navbar />
     <Main />
+    <SectionCards />
 
     <Footer />
     </>
